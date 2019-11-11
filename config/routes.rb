b4ae0f669c02d5e428059441c71c74d13027e480
Oliver Allmoslechner <allmoslechner@gmail.com>
2019-11-11 13:55:10 +0000

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # home page with form
+  get 'ask', to: 'questions#ask'
+
+  # page with answer
+  get 'answer', to: 'questions#answer'
 end
